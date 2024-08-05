1986b6c492befb6678835674d594e36f7d474289
Zacharyii <1903253513@qq.com>
2024-08-05 20:34:33 +0800

    update

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,6 +1,56 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
+/*
+ * @Author: 晨老斯
+ * @Description:一个彩票抽奖系统。
+ * @dev Implements Chainlink VRFv2.5
+ */
+
 contract Raffle {
-    constructor() {}
+    error Raffle__SendMoreToEnterRaffle();
+
+    uint256 private immutable i_entranceFee; // 抽奖费
+    address payable[] private s_players; // 抽奖用户
+
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
+
+    function pickWinner() public {}
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
